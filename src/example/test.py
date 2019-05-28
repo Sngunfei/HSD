1a39f27ca9b79456d326f72185c2819036183eb9
syfnico <syfnico@foxmail.com>
2019-05-28 18:19:44 +0800

    相似度计算

--- src/example/test.py
+++ src/example/test.py
@@ -4,25 +4,33 @@ from model import AutoEncoder
 from example import datasets
 import networkx as nx
 from utils.plt import plot_embeddings
+import numpy as np
 
-def run(scale, dataset="bell"):
+def run(scale, dataset, method):
     settings = parser.parameter_parser()
-    parser.tab_printer(settings)
+    #parser.tab_printer(settings)
 
-    data_path = datasets.edgelist[dataset]
+    #data_path = datasets.edgelist[dataset]
+    data_path = "G:\pyworkspace\graph-embedding\data\\fb1.edgelist"
     graph = nx.read_edgelist(path=data_path, create_using=nx.Graph, nodetype=str, edgetype=float, data=[('weight', float)])
 
     wavelet_model = GraphWave(graph, settings)
-    wavelet_model.dist_measure(scale)
+    #wavelet_model.embedding_similarity(dataset, scale)
+    embeddings = wavelet_model.single_scale_embedding(scale)
+
+    #wavelet_model.dist_measure(scale, method, save_path="G:\pyworkspace\graph-embedding\out\{}_{}_{}.txt".format(dataset, scale, method))
+
+    #embeddings = wavelet_model.single_scale_embedding(scale)
     #wavlet_coeffs = wavelet_model.dev_cal_all_wavelet_coeffs(scale)
     # wavelet_model.save_coeff_fig(dataset, scale, 20)
     #embeddings = AutoEncoder.create_embedding(wavlet_coeffs, d_in=wavelet_model.n_nodes)
-    #plot_embeddings(wavelet_model.nodes, embeddings, method="tsne")
+
+    plot_embeddings(wavelet_model.nodes, embeddings, method="tsne")
 
     #wavelet_model.dev_wavlet_KL(dataset, scale, save=False, fig=True, top=False)
     #wavelet_model.dev_coeff_autoencoder(scale)
 
 
 if __name__ == '__main__':
-    run(1.5, dataset="bell")
+    run(5, dataset="fb1", method="L1")
 
---
+++ src/model/LaplacianEigenmaps.py
@@ -0,0 +1,60 @@
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import scipy.io as sio
+import scipy.sparse as sp
+import scipy.sparse.linalg as lg
+from utils.plt import plot_embeddings
+
+
+class LaplacianEigenmaps:
+
+    def __init__(self, graph):
+        self.graph = graph
+        self.n_node = nx.number_of_nodes(graph)
+        self.nodes = nx.nodes(graph)
+
+
+    def create_embedding(self, d):
+        L_sym = nx.normalized_laplacian_matrix(graph)
+
+        w, v = lg.eigs(L_sym, k=d + 1, which='SM')
+        self._X = v[:, 1:]
+
+        p_d_p_t = np.dot(v, np.dot(np.diag(w), v.T))
+        eig_err = np.linalg.norm(p_d_p_t - L_sym)
+        print('Laplacian matrix recon. error (low rank): %f' % eig_err)
+        return self._X
+
+    def get_embedding(self):
+        return self._X
+
+    def get_edge_weight(self, i, j):
+        return np.exp(
+            -np.power(np.linalg.norm(self._X[i, :] - self._X[j, :]), 2)
+        )
+
+    def get_reconstructed_adj(self, X=None, node_l=None):
+        if X is not None:
+            node_num = X.shape[0]
+            self._X = X
+        else:
+            node_num = self._node_num
+        adj_mtx_r = np.zeros((node_num, node_num))
+        for v_i in range(node_num):
+            for v_j in range(node_num):
+                if v_i == v_j:
+                    continue
+                adj_mtx_r[v_i, v_j] = self.get_edge_weight(v_i, v_j)
+        return adj_mtx_r
+
+
+if __name__ == '__main__':
+    data_path = 'G:\pyworkspace\graph-embedding\out\\subway_dist_L1.txt'
+    graph = nx.read_edgelist(path=data_path, create_using=nx.Graph, edgetype=float, data=[('weight', float)])
+    model = LaplacianEigenmaps(graph)
+    embeddings = np.array(model.create_embedding(5))
+    print(embeddings.shape)
+    plot_embeddings(model.nodes, embeddings, method="pca")
+    plt.show()
