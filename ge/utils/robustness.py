e4c1dd329640350a5ca654cbfa34c0b4face7be5
syfnico <syfnico@foxmail.com>
2019-09-23 22:33:17 +0800

    delete edges randomly.

---
+++ ge/utils/robustness.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+"""
+    Test robustness for edge removel.
+"""
+import networkx as nx
+import random
+
+
+def random_remove_edges(graph=None, prob=0.9):
+    random.seed()
+    edges = nx.edges(graph)
+    del_edges = []
+    for edge in edges:
+        u, v = edge[0], edge[1]
+        if random.random() > prob:
+            del_edges.append((u, v))
+    graph.remove_edges_from(del_edges)
+    return graph
